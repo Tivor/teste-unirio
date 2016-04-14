116e285c938d9f5b268746f1f7b7bd7aad7102a1
igorvelosocustodio@gmail.com <m0d4focA>
2016-04-13 23:35:15 -0300

    laser printer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,18 @@ import java.util.concurrent.Executors;
  * Created by Poliana on 19/12/2015.
  */
 public class Main {
+    public static final int EVOLUTION_RATIO = 5;
+    public static final int REPETICOES = 1;
+    public static final int PRIMEIRO_INPUT = 15;
     static ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public static void main(String[] args) throws IOException, InvalidConfigurationException {
 
-        for (int j = 1; j >= 1; j--) {
+        for (int j = EVOLUTION_RATIO; j >= EVOLUTION_RATIO; j--) {
 
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < REPETICOES; i++) {
 
-                int fileIndex = 4;
+                int fileIndex = PRIMEIRO_INPUT;
 
                 do {
                     String featuresFile = "input/Features" + fileIndex + ".dat";
@@ -25,7 +28,7 @@ public class Main {
                     Executor executor = new Executor(featuresFile, alternativesFile, j * 1000, false, i + "_" + j);
                     pool.execute(executor);
                     fileIndex++;
-                } while (fileIndex <= 4);
+                } while (fileIndex <= 5);
 
             }
         }
